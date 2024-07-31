0cd9fb4172070725acb4021a326c4029932c5db0
배세은 <baeseeun@baeseeun-ui-MacBookAir.local>
2024-07-31 14:42:05 +0900

    Object와 다형성

---
+++ src/lang/object/tostring/ToStringMain2.java
@@ -0,0 +1,25 @@
+package lang.object.tostring;
+
+public class ToStringMain2 {
+
+    public static void main(String[] args) {
+        Car car = new Car("Model Y");
+        Dog dog = new Dog("바둑이", 2);
+        Dog dog2 = new Dog("진돌이", 5);
+
+        System.out.println("1. 단순 toString 호출");
+        System.out.println(car.toString());
+        System.out.println(dog.toString());
+        System.out.println(dog2.toString());
+
+        System.out.println("2. println 내부에서 toString 호출");
+        System.out.println(car);
+        System.out.println(dog);
+        System.out.println(dog2);
+
+        System.out.println("3. Object 다형성 활용");
+        ObjectPrinter.print(car);
+        ObjectPrinter.print(dog);
+        ObjectPrinter.print(dog2);
+    }
+}
